72b2028e11487e7c3bca6f19853ef56b948a6203
jahzz <jasonhuang3030@gmail.com>
2025-02-12 16:00:08 -0600

    replaced py4j with JEP and javafx to prep for spotify listeners

---
+++ src/main/java/LyricaL/SpotifyListener.java
@@ -0,0 +1,8 @@
+package LyricaL;
+import javafx.application.Platform;
+import javafx.concurrent.Worker;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
+public class SpotifyListener {
+    
+}
